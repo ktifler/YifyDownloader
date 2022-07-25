dedc65f6c4c2f81edf979aeb2da0017b5c1671f7
Saddam Alktif <saddamalktif@gmail.com>
2022-07-25 03:53:02 +0200

    Update YifySearch.py
    - Increased speed by using requests instead of Selenium to get download links
    - Formating with PEP 8 .

--- YifySearch.py
+++ YifySearch.py
@@ -6,10 +6,9 @@ Created on Fri Nov  6 11:35:31 2020
 """
 
 from bs4 import BeautifulSoup
+from art import tprint
 import requests
-from selenium import webdriver
 import os
-from time import sleep
 
 
 def make_url(name):
@@ -17,71 +16,68 @@ def make_url(name):
     search_query = "browse-movies/"
     movie_name = name
     #section_movie = '&section=movie'
-    other_option=""
+    other_option = ""
     final_url = URL + search_query + str(movie_name)
     return final_url
+
+
 def get_page(url):
-    hdr={'User-Agent': 'Mozilla/5.0'} #solve http 403
-    page=requests.get(url,headers=hdr)
-    soup = BeautifulSoup(page.content,'html.parser')
-    return page,soup
+    hdr = {'User-Agent': 'Mozilla/5.0'}  # solve http 403
+    page, soup = None, None
+    try:
+        page = requests.get(url, headers=hdr)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+        else:
+            raise('Unable to process web page')
+    except Exception as e:
+        print(f'Unable to access {url}\nError: {e}')
+    return page, soup
 
 
 def get_movies_names(name):
-    result_dict=dict()
-    page,soup = get_page(make_url(name))
-    query_results = soup.find_all("div",class_="browse-movie-wrap col-xs-10 col-sm-4 col-md-5 col-lg-4")
-    for entity in query_results:
-        entity1=entity.find("div",class_="browse-movie-bottom").a
-        result_dict[entity1.string]=entity1['href']
-    return result_dict
+    _, soup = get_page(make_url(name))
+    result_links = soup.find_all(
+        "a", class_="browse-movie-title")
+    return {movie_div.text: movie_div['href'] for movie_div in result_links}
 
 
 def get_donwload_url(url):
-    options1 = webdriver.ChromeOptions()
-    options1.add_argument('--ignore-certificate-errors')
-    options1.add_argument('--disable-gpu')
-    options1.add_argument('--incognito')
-    options1.add_argument('--headless')
-    DRIVER = webdriver.Chrome(options=options1)
-    DRIVER.get(url)
-    link_text=DRIVER.find_elements_by_link_text("Download")
-    print(link_text)
-    if len(link_text)>0:
-        link_text[0].click()
-    tabs = DRIVER.window_handles
-    DRIVER.switch_to.window(tabs[0])
-    print('(X]Current url ----->', DRIVER.current_url)
-    page_source = DRIVER.page_source
-    DRIVER.quit()
-    soup=BeautifulSoup(page_source,'html.parser')
-    div=soup.find_all('div',class_="modal-torrent")
-    return {' '.join(item.text.strip().split('\n')[:-2]):[a['href'] for a in item.find_all("a")] for item in div}
+    _, soup = get_page(url)
+    p = soup.select_one('#movie-info > p')
+    return {a.text: a['href'] for a in p.find_all("a")}
 
-#with default torrent client
+# with default torrent client
+
+
 def donwload_magnet(magnet):
     return os.startfile(magnet)
 
-def main():
-    name=str(input("search a movie\t"))
-    result_dict=get_movies_names(name)
-    name_list=list(result_dict.keys())
-    href_list=list(result_dict.values())
-    for i,title in enumerate(name_list):
-        if title != None:
-            print('{:4} {:50}'.format(i,title))
-    index=int(input('choose a number to fetch download links'))
-    href=href_list[index]
-    print(name_list[index],href)
-    q=str(input('press y to download\t'))
-    if q=='y':
-        qualities_dict=get_donwload_url(href)
-        for i,qu in enumerate(list(qualities_dict.keys())):
-            print('{:4} {:50}'.format(i,qu))
-        indexx=int(input('choose a quality'))
-        magnet=list(qualities_dict.values())[indexx][1]
-        print(magnet)
-        donwload_magnet(magnet)
 
-if __name__=='__main__':
+def main():
+    tprint('YIFY Downloader')
+    name = str(input("search a movie\t"))
+    result_dict = get_movies_names(name)
+    names_list = list(result_dict.keys())
+    links_list = list(result_dict.values())
+    for i, title in enumerate(names_list):
+        if title != None:
+            print('{:4} {:50}'.format(i, title))
+
+    choice = int(input('choose a number to fetch download links'))
+    href = links_list[choice]
+    print(names_list[choice], href)
+    validate_download = str(input('press y to download\t'))
+    if validate_download == 'y':
+        qualities_dict = get_donwload_url(href)
+        for i, quality in enumerate(list(qualities_dict.keys())):
+            print('{:4} {:50}'.format(i, quality))
+        quality_choice = int(input('choose a quality'))
+        magnet_link = list(qualities_dict.values())[quality_choice]
+        print(f'Torrent link{magnet_link}')
+        print('If you have installed a torrent client, a download prompt will appear')
+        donwload_magnet(magnet_link)
+
+
+if __name__ == '__main__':
     main()
